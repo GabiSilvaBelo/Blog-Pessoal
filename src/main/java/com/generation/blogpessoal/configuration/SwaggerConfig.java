31719b69874c3fdcf9eb5d88aec6dcdd1cd9138a
GabiSilvaBelo <gabybelosp@gmail.com>
2023-03-31 14:44:10 -0300

    Deploy do Projeto Blog Pessoal 2

--- src/main/java/com/generation/blogpessoal/configuration/SwaggerConfig.java
+++ src/main/java/com/generation/blogpessoal/configuration/SwaggerConfig.java
@@ -26,7 +26,7 @@ public class SwaggerConfig {
 					.name("Generation Brasil")
 					.url("https://brazil.generation.org/"))
 				.contact(new Contact()
-					.name("Conteudo Generation")
+					.name("Blog Pessoal")
 					.url("https://github.com/GabiSilvaBelo/Blog-Pessoal")
 					.email("gabybelosp@gmail.com")))
 				.externalDocs(new ExternalDocumentation()
@@ -59,5 +59,6 @@ public class SwaggerConfig {
 		return new ApiResponse().description(message);
 
 	}
+	
 
 }
